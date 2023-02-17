7740cc6d29c8e4a65521a17f4c5922767dd38219
Balázs Blasek <blasek.balazs@gmail.com>
2023-02-17 11:11:26 +0100

    email bugfix

--- Web/HouseOfSwords/app/Mail/MailNotify.php
+++ Web/HouseOfSwords/app/Mail/MailNotify.php
@@ -33,7 +33,6 @@ class MailNotify extends Mailable
     public function envelope()
     {
         return new Envelope(
-            from: new Address('blasekb@houseofswords.hu', 'House of Swords'),
             subject: 'Test email',
         );
     }
