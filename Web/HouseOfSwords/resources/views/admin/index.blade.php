c2a5bad5e6e926f6679a173ef91b477d5a35a34d
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-02-20 09:59:08 +0100

    Admin page - table

--- Web/HouseOfSwords/resources/views/admin/index.blade.php
+++ Web/HouseOfSwords/resources/views/admin/index.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="/css/table.css">
 @section('content')
-
-<div class="row bg-text mt-3">
-    <div class="col-12">
-        <h1 class="text-center display-3">Egy admin oldal...</h1>
+    <div class="row bg-text mt-3">
+        <div class="col-12">
+            <h1 class="text-center display-3">Egy admin oldal...</h1>
+        </div>
     </div>
-</div>
 
-<div class="container">
+    {{-- <div class="container">
     <div class="row">
         @foreach ($bugs as $bug)
         <div class="card my-3 mx-3" style="width: 18rem;">
@@ -20,6 +19,49 @@
           </div>
         @endforeach
     </div>
-</div>
+</div> --}}
 
+<div class="container">
+
+    <h2 class="mb-5">Hibák</h2>
+
+
+    <div class="table-responsive">
+
+        <table class="table table-striped custom-table">
+            <thead>
+                <tr>
+
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Occupation</th>
+                    <th scope="col">Contact</th>
+                    <th scope="col">Education</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+        @foreach ($bugs as $bug)
+
+                <tr scope="row">
+                    <td>
+                        {{$bug->Id}}
+                    </td>
+                    <td><a href="#">user-email</a></td>
+                    <td>
+                        Bug text:
+                        <small class="d-block">{{$bug->Text}}</small>
+                    </td>
+                    <td>+63 983 0962 971</td>
+                    <td>NY University</td>
+                    <td><a href="#" class="more">Details</a></td>
+
+                </tr>
+        @endforeach
+
+
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
