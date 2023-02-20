9a420070c73924ec06b6e35114d7dfe97fa5af9e
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-02-20 12:10:11 +0100

    Admin page- table done

--- Web/HouseOfSwords/resources/views/admin/index.blade.php
+++ Web/HouseOfSwords/resources/views/admin/index.blade.php
@@ -1,67 +1,57 @@
 @extends('layouts.app')
 <link rel="stylesheet" href="/css/table.css">
 @section('content')
-    <div class="row bg-text mt-3">
+    {{-- <div class="row bg-text mt-3">
         <div class="col-12">
             <h1 class="text-center display-3">Egy admin oldal...</h1>
         </div>
+    </div> --}}
+
+    <div class="container py-5">
+        <h2 class="mb-3 text-center">Hibák</h2>
+        <div class="table-responsive">
+            <table class="table table-striped custom-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Occupation</th>
+                        <th scope="col">Hiba jelenlegi státusza</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr scope="row">
+                        <td>00</td>
+                        <td><a href="#">user@email.com</a></td>
+                        <td><small class="d-block">Valami</small></td>
+                        <td class="status"><span class="bug">Bug</span></td>
+                        <td>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            </div>
+                        </td>
+                    </tr>
+
+                    @foreach ($bugs as $bug)
+                        <tr scope="row">
+                            <td>
+                                {{ $bug->Id }}
+                            </td>
+                            <td><a href="#">user@email.com</a></td>
+                            <td><small class="d-block">{{ $bug->Text }}</small></td>
+                            <td class="status"><span class="fixed">Feature</span></td>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        id="flexSwitchCheckDefault">
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
     </div>
-
-    {{-- <div class="container">
-    <div class="row">
-        @foreach ($bugs as $bug)
-        <div class="card my-3 mx-3" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Bug:{{$bug->Id}}</h5>
-              <p class="card-text">{{$bug->Text}}</p>
-              <a href="" class="btn btn-primary disabled">Probléma megoldva</a>
-            </div>
-          </div>
-        @endforeach
-    </div>
-</div> --}}
-
-<div class="container">
-
-    <h2 class="mb-5">Hibák</h2>
-
-
-    <div class="table-responsive">
-
-        <table class="table table-striped custom-table">
-            <thead>
-                <tr>
-
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Occupation</th>
-                    <th scope="col">Contact</th>
-                    <th scope="col">Education</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-        @foreach ($bugs as $bug)
-
-                <tr scope="row">
-                    <td>
-                        {{$bug->Id}}
-                    </td>
-                    <td><a href="#">user-email</a></td>
-                    <td>
-                        Bug text:
-                        <small class="d-block">{{$bug->Text}}</small>
-                    </td>
-                    <td>+63 983 0962 971</td>
-                    <td>NY University</td>
-                    <td><a href="#" class="more">Details</a></td>
-
-                </tr>
-        @endforeach
-
-
-            </tbody>
-        </table>
-    </div>
-</div>
 @endsection
