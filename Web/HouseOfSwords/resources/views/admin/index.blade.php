3743ec4797984b10efaad0af35e4ddc8df2fbacb
Torpe17 <laura.luksa03@gmail.com>
2023-02-21 21:15:46 +0100

    table oszlop szélesség

--- Web/HouseOfSwords/resources/views/admin/index.blade.php
+++ Web/HouseOfSwords/resources/views/admin/index.blade.php
@@ -13,11 +13,11 @@
             <table class="table table-striped custom-table">
                 <thead>
                     <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Occupation</th>
-                        <th scope="col">Hiba jelenlegi státusza</th>
-                        <th scope="col"></th>
+                        <th scope="col" width="5%">Id</th>
+                        <th scope="col" width="20%">Email</th>
+                        <th scope="col" width="40%">Occupation</th>
+                        <th scope="col" width="20%" class=" text-center">Hiba státusza</th>
+                        <th scope="col" width="5%"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -25,7 +25,7 @@
                         <td>00</td>
                         <td><a href="#">user@email.com</a></td>
                         <td><small class="d-block">Valami</small></td>
-                        <td class="status"><span class="bug">Bug</span></td>
+                        <td class="status text-center"><span class="bug">Megoldatlan</span></td>
                         <td>
                             <div class="form-check form-switch">
                                 <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
@@ -40,7 +40,7 @@
                             </td>
                             <td><a href="#">user@email.com</a></td>
                             <td><small class="d-block">{{ $bug->Text }}</small></td>
-                            <td class="status"><span class="fixed">Feature</span></td>
+                            <td class="status text-center"><span class="fixed">Megoldva</span></td>
                             <td>
                                 <div class="form-check form-switch">
                                     <input class="form-check-input" type="checkbox" role="switch"
