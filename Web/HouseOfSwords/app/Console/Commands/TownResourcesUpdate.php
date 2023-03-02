9cd7948b8d0f1ad2f28a4351b369f14e14828b2d
Torpe17 <laura.luksa03@gmail.com>
2023-03-02 20:32:56 +0100

    TownResourcesUpdate

--- Web/HouseOfSwords/app/Console/Commands/TownResourcesUpdate.php
+++ Web/HouseOfSwords/app/Console/Commands/TownResourcesUpdate.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Building;
+use App\Models\Buildings\Warehouse;
 use Illuminate\Console\Command;
 use App\Models\Town;
 
@@ -30,11 +32,23 @@ class TownResourcesUpdate extends Command
     {
         $towns = Town::all();
 
+        error_log($towns);
+        error_log('towns');
         foreach ($towns as $key => $town) {
-            $town->Wood += 1;
-            $town->Stone += 1;
-            $town->Metal += 1;
-            $town->Gold += 1;
+            $buildings = Building::where([
+                ['Towns_TownID', '=', $town->TownID],
+                ['BuildingType', '=', 'Warehouse'],
+            ]);
+            error_log($buildings);
+            error_log('buildings');
+
+            $resourcheStats = Warehouse::find($buildings->BuildingLvl);
+            error_log('resourcheStats');
+            error_log($resourcheStats);
+            $town->Wood += $town->BrigadeInWood * $resourcheStats->WoodCollectionPM;
+            $town->Stone += $town->BrigadeInStone * $resourcheStats->StoneCollectionPM;
+            $town->Metal += $town->BrigadeInMetal * $resourcheStats->MetalCollectionPM;
+            $town->Gold += $town->BrigadeInGold * $resourcheStats->GoldCollectionPM;
             $town->save();
         }
 
