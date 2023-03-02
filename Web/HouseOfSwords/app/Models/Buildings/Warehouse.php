60b968bdda496df0702a1dbc8ae6d7f36131d811
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-03-02 14:24:38 +0100

    Update Warehouse.php

--- Web/HouseOfSwords/app/Models/Buildings/Warehouse.php
+++ Web/HouseOfSwords/app/Models/Buildings/Warehouse.php
@@ -10,7 +10,7 @@ class Warehouse extends Model
     use HasFactory;
 
     // table properties
-    protected $table = 'warehousestats';
+    protected $table = 'levelstats_warehouse';
     protected $primaryKey = 'Lvl';
     public $timestamps = false;
 
