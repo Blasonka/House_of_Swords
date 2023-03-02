270eef1ab033feb0a094f274274bb8d7a4fdbf67
Torpe17 <laura.luksa03@gmail.com>
2023-03-02 21:13:11 +0100

    TownResourcesUpdate done

--- Web/HouseOfSwords/app/Console/Commands/TownResourcesUpdate.php
+++ Web/HouseOfSwords/app/Console/Commands/TownResourcesUpdate.php
@@ -32,23 +32,17 @@ class TownResourcesUpdate extends Command
     {
         $towns = Town::all();
 
-        error_log($towns);
-        error_log('towns');
         foreach ($towns as $key => $town) {
             $buildings = Building::where([
                 ['Towns_TownID', '=', $town->TownID],
                 ['BuildingType', '=', 'Warehouse'],
-            ]);
-            error_log($buildings);
-            error_log('buildings');
+            ])->get()->first();
 
             $resourcheStats = Warehouse::find($buildings->BuildingLvl);
-            error_log('resourcheStats');
-            error_log($resourcheStats);
-            $town->Wood += $town->BrigadeInWood * $resourcheStats->WoodCollectionPM;
-            $town->Stone += $town->BrigadeInStone * $resourcheStats->StoneCollectionPM;
-            $town->Metal += $town->BrigadeInMetal * $resourcheStats->MetalCollectionPM;
-            $town->Gold += $town->BrigadeInGold * $resourcheStats->GoldCollectionPM;
+            $town->Wood += $buildings->BrigadeInWood * $resourcheStats->WoodCollectionPM;
+            $town->Stone += $buildings->BrigadeInStone * $resourcheStats->StoneCollectionPM;
+            $town->Metal += $buildings->BrigadeInMetal * $resourcheStats->MetalCollectionPM;
+            $town->Gold += $buildings->BrigadeInGold * $resourcheStats->GoldCollectionPM;
             $town->save();
         }
 
