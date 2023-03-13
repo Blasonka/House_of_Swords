7423976ebebbec11252ab965135857701c46d922
Balázs Blasek <blasek.balazs@gmail.com>
2023-03-13 11:34:23 +0100

    helyesírás és datetime bugreportnál

--- Web/HouseOfSwords/app/Http/Controllers/MailController.php
+++ Web/HouseOfSwords/app/Http/Controllers/MailController.php
@@ -63,7 +63,7 @@ class MailController extends Controller
         };
 
         try {
-            Bugreport::create(["Text" => $request->Text, 'EmailAddress' => $data->email]);
+            Bugreport::create(["Text" => $request->Text, 'EmailAddress' => $data->email, 'Date' => now()]);
             Mail::to('info@houseofswords.hu')->send(new BugReportEmail($data));
             return redirect('/bugreport')->with('status', 'Jelentés sikeresen elküldve!');
         } catch (Exception $err) {
